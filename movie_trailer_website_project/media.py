8df0e81a2c61b0fcf6ba47e94b13d6577eddee3a
bjoshi1702 <30448367+bjoshi1702@users.noreply.github.com>
2018-03-03 23:52:25 -0800

    last update in media file

--- movie_trailer_website_project/media.py
+++ movie_trailer_website_project/media.py
@@ -3,12 +3,10 @@ class Movie():
     """ Movie class takes information which suppose to show on website
 
         Attributes:
-        
         movie_title (str): give name of the movie
         movie_storyline (str): give storyline of the movie
         poster_image (str): provide link for movie poster
         trailer_youtube (str): provide youtube movie trailer link
-        
     """
     def __init__(self, movie_title, movie_storyline,
                  poster_image, trailer_youtube):
@@ -16,4 +14,3 @@ class Movie():
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-        
